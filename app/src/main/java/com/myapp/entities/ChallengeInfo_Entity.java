4d9de11d38511d29e98f86ab3f80ed623c1b5c22
PhoenixOfDoom <B.1.11.11@hotmail.com>
2024-01-01 18:38:25 +0200

    Challenge Entity added!

---
+++ app/src/main/java/com/myapp/entities/ChallengeInfo_Entity.java
@@ -0,0 +1,21 @@
+package com.myapp.entities;
+
+import java.sql.Time;
+
+public class ChallengeInfo_Entity {
+    public Time Challenge_Duration;
+    public Time Challenge_Initalize_Time;
+    // TODO - will be computed through initialize!
+    public Time Challenge_Deadline;
+    public int Challenge_Id;
+    public String Challenge_Host_username;
+    public boolean is_public;
+    public boolean is_accomplished;
+    public boolean is_active;
+
+    //Challenge Files Data
+    public String Path_To_Host_Video;
+    public String Path_To_Participant_Video;
+
+    public boolean File_Status;
+}
