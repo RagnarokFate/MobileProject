36e7983f9fb944f74466ddc8f87f47408377a312
PhoenixOfDoom <B.1.11.11@hotmail.com>
2024-01-01 18:57:11 +0200

    Data Transfer from Client - user

---
+++ app/src/main/java/com/myapp/entities/Request_From_Server.java
@@ -0,0 +1,15 @@
+package com.myapp.entities;
+
+public class Request_From_Server {
+    public int request_id;
+    public int user_request_id;
+
+    public String username;
+
+    public Object Data_received;
+
+    public Object Data_sent;
+
+    public Object message;
+
+}
---
+++ app/src/main/java/com/myapp/entities/Request_To_Server.java
@@ -0,0 +1,14 @@
+package com.myapp.entities;
+
+public class Request_To_Server {
+    public int request_id;
+    public int user_request_id;
+
+    public String username;
+
+    public Object Data_received;
+
+    public Object Data_sent;
+
+    public Object message;
+}
